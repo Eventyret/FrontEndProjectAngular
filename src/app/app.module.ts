0710cbcf5bc9b5fe87d0b7c9306d4e9a2ae6c422
Simen Daehlin <simen.dehlin@gmail.com>
2017-10-18 21:40:12 +0100

    Added Http Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { HttpModule } from '@angular/http';
+import { SeriesService } from './services/series.service';
+import { MoviesService } from './services/movies.service';
+import { OmdbService } from './services/omdb.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,9 +14,14 @@ import { MovieListComponent } from './movie-list/movie-list.component';
     MovieListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    OmdbService,
+    MoviesService,
+    SeriesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
