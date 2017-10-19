a28bf1a3da38134d0da76b97d45cbf868ebe39d8
Simen Daehlin <simen.dehlin@gmail.com>
2017-10-19 17:24:41 +0100

    Added a singlelist display

---
+++ src/app/single-display/single-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-display',
+  templateUrl: './single-display.component.html',
+  styleUrls: ['./single-display.component.css']
+})
+export class SingleDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
