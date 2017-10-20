7fc584b5e4a33e3fae058f7a53fcbc194eae8488
Simen Daehlin <simen.dehlin@greenglowltd.com>
2017-10-20 16:44:18 +0100

    Added 404 Page not found, startedmockup of layout of search box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,20 @@ import { MovieListComponent } from './movie-list/movie-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SingleDisplayComponent } from './single-display/single-display.component';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFound404Component } from './page-not-found-404/page-not-found-404.component';
 
 const appRoutes: Routes = [
-  {path:'', component:MovieListComponent},
-  {path:'info', component:SingleDisplayComponent}
+  {path: '', component: MovieListComponent},
+  {path: 'info', component:SingleDisplayComponent},
+  {path: '404', component: PageNotFound404Component}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
-    SingleDisplayComponent
+    SingleDisplayComponent,
+    PageNotFound404Component
   ],
   imports: [
     BrowserModule,
