07ccd6ba6e3bd6eb8a6157e257d11f65ea7962a3
Simen Daehlin <simen.dehlin@gmail.com>
2017-10-28 00:35:12 +0100

    Cards to main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SingleDisplayComponent } from './components/single-display/single-display.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFound404Component } from './components/page-not-found-404/page-not-found-404.component';
-import { CapitalizePipe } from './trim.pipe';
+import { CapitalizePipe } from './capitalize.pipe';
 import { ScrollToModule } from 'ng2-scroll-to-el';
+import { CardStyleComponent } from './components/card-style/card-style.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const appRoutes: Routes = [
-  {path: '', component: MovieListComponent},
+  { path: '', component: CardStyleComponent},
+  {path: 'list-view', component: MovieListComponent},
   {path: 'info', component:SingleDisplayComponent},
   {path: '404', component: PageNotFound404Component}
 ]
@@ -27,6 +30,7 @@ const appRoutes: Routes = [
     SingleDisplayComponent,
     PageNotFound404Component,
     CapitalizePipe,
+    CardStyleComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,7 +39,8 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     TruncateModule,
     RouterModule.forRoot(appRoutes),
-    ScrollToModule.forRoot()
+    ScrollToModule.forRoot(),
+    NgbModule.forRoot()
 
   ],
   providers: [
