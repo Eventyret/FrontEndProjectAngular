976c033ede6a58d83c1da2c48388717366aacf2d
Simen Daehlin <simen.dehlin@gmail.com>
2017-10-28 00:33:59 +0100

    Added Card component

---
+++ src/app/components/card-style/card-style.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardStyleComponent } from './card-style.component';
+
+describe('CardStyleComponent', () => {
+  let component: CardStyleComponent;
+  let fixture: ComponentFixture<CardStyleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CardStyleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardStyleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
