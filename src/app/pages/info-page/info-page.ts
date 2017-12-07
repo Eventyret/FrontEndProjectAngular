c28ab7a1c33b9ef88736c0a4a723c1e075eb89e9
Simen Daehlin <simen.dehlin@gmail.com>
2017-12-07 23:08:42 +0000

    Cleaned up and started with error handling for info-page

--- src/app/pages/info-page/info-page.ts
+++ src/app/pages/info-page/info-page.ts
@@ -1,76 +1,70 @@
-import { SearchService } from '../../services/search.service';
-import { Component, OnInit } from '@angular/core';
+import { Response } from "@angular/http";
+import { SearchService } from "../../services/search.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'info-page',
-  templateUrl: './info-page.html',
-  styleUrls: ['./info-page.css']
+  selector: "info-page",
+  templateUrl: "./info-page.html",
+  styleUrls: ["./info-page.css"]
 })
 export class SingleDisplayComponent implements OnInit {
+  constructor(private searchService: SearchService) {}
   // Different Arrays to hold data
   movie: any[];
   artwork: any[];
   posters: any[];
-  actors: any[]
-  genres: any[]
-  ratings: any[]
+  actors: any[];
+  genres: any[];
+  ratings: any[];
   Title: string;
 
   // Storing data to use in Single-Display Component
-  imdbID: string = sessionStorage.getItem('imdbID');
-  type: string = sessionStorage.getItem('type');
-  movieInfo:any
+  imdbID: string = sessionStorage.getItem("imdbID");
+  type: string = sessionStorage.getItem("type");
+  movieInfo: any;
   // Single outputs from arrays
   Poster: string;
   backgroundimage: string;
   slashgenres: string;
   slashlang: string;
 
-// Error & Status Messages Messages (Needs cleanup)
+  // Error & Status Messages Messages (Needs cleanup)
   errorMsg: string;
-  showSpinner: boolean = true;
-  extrainfo: any[];;
-  cast: any[]
-  loaded: boolean = false;
+  showSpinner = true;
+  extrainfo: any[];
+  cast: any[];
+  loaded = false;
 
+  // Lets grab some extra information for this movie.
+  ngOnInit() {
+    if (localStorage["movieInfo"]) {
+      this.movieInfo = JSON.parse(sessionStorage.getItem("movieInfo"));
+    } else {
+      console.log("Its undefined");
+    }
+  }
 
-  constructor(private searchService: SearchService) {
-      this.searchService.getSingleMovie(this.imdbID).subscribe(movie => {
-      this.movie = movie;
-      this.Title = movie.Title
-      this.actors = movie.Actors.split(",");
-      this.slashlang = movie.Language.replace(/,/g, " /");
-      this.ratings = movie.Ratings
-      this.showSpinner = false;
-    }),
-    (error)=> {
-
-            }, 
-            () => {
-                console.log("Single Movie Loading Complete")
-}
-// Lets grab some extra information for this movie.
-    this.searchService.getExtraMovieInfo(this.imdbID).subscribe(extrainfo => {
-      this.extrainfo = extrainfo
-      this.cast = extrainfo.cast
-      console.log(this.extrainfo);
-      this.showSpinner = false;
-      this.loaded = true;
-    }),
-      (error) => {
-        console.log(error)
+  singleMovie() {
+    this.searchService.getSingleMovie(this.imdbID).subscribe(
+      movie => {
+        this.movie = movie;
+        this.Title = movie.Title;
+        this.actors = movie.Actors.split(",");
+        this.slashlang = movie.Language.replace(/,/g, " /");
+        this.ratings = movie.Ratings;
+        this.showSpinner = false;
+      },
+      (error: Response) => {
+        if(error.status === 404){
+          alert("Error 404")
+        } else {
+          alert("An unexpected error occured");
+          console.log(error);
+        }
       }
+    );
   }
   handleSuccess(data) {
     this.movie = data;
   }
-  ngOnInit() {
-    this.imdbID;
-    if (localStorage["movieInfo"]) {
-      this.movieInfo = JSON.parse(sessionStorage.getItem('movieInfo'));
-    } else {
-      console.log("Its undefined")
-    }
-  }
-
 }
