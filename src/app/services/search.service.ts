667fbbf9a5ee8e432612848cc8ad7f71878e4fbc
Simen Daehlin <simen.dehlin@gmail.com>
2017-12-07 23:08:14 +0000

    Removed Sonarr as it is not used for now. Removed old error handler. Also cleaned up code

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -3,8 +3,6 @@ import { environment } from "../../environments/environment";
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
-import "rxjs/add/operator/catch";
-import "rxjs/add/observable/throw";
 import _ from "lodash";
 
 @Injectable()
@@ -17,70 +15,24 @@ export class SearchService {
   private RADARR_KEY: string = environment.RADARR_KEY;
   private RADAR_URL: string = environment.RADARR_URL;
   private RADAR_STRING: string = "?apikey=" + this.RADARR_KEY;
-  private SONARR_KEY: string = environment.SONARR_KEY;
-  private SONARR_URL: string = environment.SONARR_URL;
-  private SONARR_STRING: string = "?apikey=" + this.SONARR_KEY;
-  private movie_url: string = environment.IMDB_MOVIE_URL;
-  private actor_url: string = environment.IMDB_ACTOR_URL;
-  private endstring_actor = "?person_id=";
-  private endstring_movie = "?movie_id=";
-  private movieID: string;
-  private actorID: string;
 
   constructor(private http: Http) {}
-
-  private handleError(err) {
-    let errMessage: string;
-
-    if (err instanceof Response) {
-      const body = err.json() || "";
-      const  error = body.error || JSON.stringify(body);
-      errMessage = `${err.status}  ${err.statusText || ""} ${error}`;
-    } else {
-      errMessage = err.message ? err.message : err.toString();
-    }
-    return Observable.throw(errMessage);
-  }
-
   getMovies(query) {
     return this.http
       .get(this.OMDB_URL + "?s=" + query + this.OMDB_STRING + "&type=movie")
       .map(res => res.json())
-      .catch(this.handleError);
+
   }
 
   getSingleMovie(imdbID) {
     return this.http
       .get(this.OMDB_URL + "?i=" + imdbID + this.OMDB_STRING + "&plot=full")
       .map(res => res.json())
-      .catch(this.handleError);
   }
 
   checkMovies() {
     return this.http
       .get(this.RADAR_URL + this.RADAR_STRING)
       .map(res => res.json())
-      .catch(this.handleError);
-  }
-
-  checkSeries() {
-    return this.http
-      .get(this.SONARR_URL + this.SONARR_STRING)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-
-  getExtraMovieInfo(movieID) {
-    return this.http
-      .get(this.movie_url + this.endstring_movie + movieID)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-
-  getActorInfo(actorID) {
-    return this.http
-      .get(this.actor_url + this.endstring_actor + this.actorID)
-      .map(res => res.json())
-      .catch(this.handleError);
   }
 }
